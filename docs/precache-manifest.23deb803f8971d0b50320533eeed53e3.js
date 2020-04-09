25296e0d431e4856f8eff8f0e70f898f6cefe1e2
DalerLeo <dalerleo@gmail.com>
2020-04-10 00:04:37 +0500

    Production

---
+++ docs/precache-manifest.23deb803f8971d0b50320533eeed53e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a802a219c8020d003e58c2b1d70470",
+    "url": "/uz-covid19-map/index.html"
+  },
+  {
+    "revision": "0ba6c3aa5f2036e3810b",
+    "url": "/uz-covid19-map/static/css/main.959f463a.chunk.css"
+  },
+  {
+    "revision": "427cbf1a2e4cb0398b83",
+    "url": "/uz-covid19-map/static/js/2.8d6913a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/uz-covid19-map/static/js/2.8d6913a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba6c3aa5f2036e3810b",
+    "url": "/uz-covid19-map/static/js/main.8acb1c7e.chunk.js"
+  },
+  {
+    "revision": "edaf68eefae18e360f11",
+    "url": "/uz-covid19-map/static/js/runtime-main.a2f830c3.js"
+  }
+]);
